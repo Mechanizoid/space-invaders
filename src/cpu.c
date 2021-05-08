f54ead034da476920e2ef29ebc0e2ffe3e4a9dbf
Christopher Orion Phoenix <orionphoenix93@gmail.com>
2021-05-07 20:53:17 -0700

    Added the first few opcodes.

--- src/cpu.c
+++ src/cpu.c
@@ -4,6 +4,9 @@
 #include "interface.h"
 #include <stdint.h>
 
+/* paramaterized macros, yeeeee */
+#define REG_PAIR(r, p)  ((((uint16_t) (r)) << 8) | ((uint16_t) (p)))
+
 
 struct Flags {
 	uint8_t z : 1;      // Zero flag
@@ -16,33 +19,50 @@ struct Flags {
 
 
 struct ProcessorState {
-	uint8_t a;
+	uint8_t accumulator;            
 	uint8_t b;
 	uint8_t c;
 	uint8_t d;
 	uint8_t e;
 	uint8_t h;
 	uint8_t l;
-	uint16_t pc;
-	uint16_t sp;
-	uint8_t *memory;
+	uint16_t program_counter;
+	uint16_t stack_pointer;
 	struct Flags flags;
-};
+} state;
 
 
 
-void process_instruction(struct ProcessorState *state) {
-	uint8_t *instruction = state->memory[state->pc];
+void process_instruction(void) {
+	uint8_t instruction = read_memory(state.program_counter);
+	uint16_t pair;
 	
 
 	switch(instruction) {
 	case 0x00:                // NOP
 		break;
-	case 0x01:                // LXI B, xxxx
-		state->b = instruction[2];
-		state->c = instruction[1];
+	case 0x01:                // LXI B, D16
+		state->b = read_memory(state.program_counter + 2);
+		state->c = read_memory(state.program_counter + 1);
 		state->pc += 2;
-		break;		
+		break;
+	case 0x02:                // STAX B
+	        pair = ((uint16_t) state.b << 8) | (uint16_t) state.c;
+		write_memory(pair, state.a);
+		state.program_counter++;
+		break;
+	case 0x03:                // INX B
+		pair = ((uint16_t) state.b << 8) | (uint16_t) state.c;
+		pair += 1;
+		state.b = (uint8_t) pair >> 8;
+		state.c = (uint8_t) (pair ^ 0x00FF);
+		break;
+	case 0x04:
+		
+		
+			
+		
+		
 	}
 
 	// advance to next opcode
