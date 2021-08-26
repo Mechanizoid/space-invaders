c68923df768c4d2ae12f9123273d2636aec7b3ad
Christopher Orion Phoenix <orionphoenix93@gmail.com>
2021-08-26 08:17:07 -0700

    Implementing the first three instructions used by Space Invaders

--- src/i8080.c
+++ src/i8080.c
@@ -20,17 +20,33 @@ void unimplemented_instruction(i8080 *state) {
  */
 void execute(i8080 *state)
 {
-	uint8_t opcode = state->memory[state->pc];
+	uint8_t *opcode = &state->memory[state->pc];
 	
-	switch (opcode) {
+	switch (*opcode) {
+	
+		/* data transfer group */
+	case 0x31: // LXI
+		state->sp = (opcode[2] << 8) | (opcode[1]);
+		state->pc += 2;
+		break;
+		
+		/* branch statements */
+	case 0xc3: // JMP
+		state->pc = (opcode[2] << 8) | (opcode[1]);
+		state->pc -= 1;
+		
+		break;
+
+		/* stack, IO, and machine control group */
 	case 0x00: // NOP
 		// does nothing
 		break;
+
 		
-	default:   // opcode is undefined
+	default:   /* unimplemented opcodes */
 		unimplemented_instruction(state);
 		break;
 	}
 
-	state->pc++;
+	state->pc += 1;
 }
