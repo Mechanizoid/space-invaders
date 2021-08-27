080f6c9399c893b4e3dbf0df6a00c62d7379c7ac
Christopher Orion Phoenix <orionphoenix93@gmail.com>
2021-08-26 23:23:15 -0700

    Implementing more 8080 instructions.
    Running the Space Invaders ROM results in the emulator traversing the code
    until it encounters an unimplemented opcode. At this point, it prints an
    error message and disassembly of the unknown instruction. Then I implement
    that instruction and run the code again.

--- src/i8080.c
+++ src/i8080.c
@@ -21,32 +21,53 @@ void unimplemented_instruction(i8080 *state) {
 void execute(i8080 *state)
 {
 	uint8_t *opcode = &state->memory[state->pc];
+	uint16_t addr;
 	
 	switch (*opcode) {
 	
 		/* data transfer group */
-	case 0x31: // LXI
-		state->sp = (opcode[2] << 8) | (opcode[1]);
+	case 0x06: // MVI B D8
+		addr = (state->h << 8) | state->l;
+		state->memory[addr] = opcode[1];
 		state->pc += 2;
 		break;
+
+	case 0x11: // LXI D
+		state->d = opcode[2];
+		state->e = opcode[1];
+
+		state->pc +=3;
+		break;
 		
+	case 0x31: // LXI SP
+		state->sp = (opcode[2] << 8) | opcode[1];
+		state->pc += 3;
+		break;
+
 		/* branch statements */
 	case 0xc3: // JMP
-		state->pc = (opcode[2] << 8) | (opcode[1]);
-		state->pc -= 1;
-		
+		state->pc = (opcode[2] << 8) | opcode[1];
+		break;
+
+	case 0xcd: // CALL
+		state->memory[state->sp - 2] = (state->pc >> 8);
+		state->memory[state->sp - 1] = (state->pc & 0x0F);
+		state->sp -= 2;
+		state->pc += 2;
+
+		state->pc = (opcode[2] << 8) | opcode[1];
+
 		break;
 
 		/* stack, IO, and machine control group */
 	case 0x00: // NOP
 		// does nothing
+		state->pc++;
 		break;
-
 		
 	default:   /* unimplemented opcodes */
 		unimplemented_instruction(state);
 		break;
 	}
 
-	state->pc += 1;
 }
